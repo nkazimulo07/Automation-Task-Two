ea5ccb59afe2925b74fe7a5295a7aefd7c93a617
Nkazimulo Siyaya <nkazimulo.siyaya@kcpivot.co.za>
2022-04-06 10:18:59 +0200

    last minutes changes

---
+++ source/repos/AutomatinTastTwo/AutomatinTastTwo/Pages/BasePage.cs
@@ -0,0 +1,14 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutomatinTastTwo
+{
+    [TestFixture]
+
+    public class BasePage
+    {
+        private IWebDriver driver;
+    }
+}
